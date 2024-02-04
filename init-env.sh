cf7f4706c2ae6ad7834c22ee82261bdc15fa099c
Bastien Goulet <bastien.goulet@uqar.ca>
2024-02-04 16:14:41 -0500

    Final sharding

--- init-env.sh
+++ init-env.sh
@@ -22,6 +22,15 @@ docker container rm postgres-standby-3
 docker container stop postgres-standby-4
 docker container rm postgres-standby-4
 
+docker container stop primary
+docker container rm primary
+
+docker container stop node1
+docker container rm node1
+
+docker container stop node2
+docker container rm node2
+
 docker volume rm postgres_primary_volume
 
 docker volume create --name postgres_primary_volume -d local
--- sharding/source/initdb.sh
+++ sharding/source/initdb.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+sleep 10
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB"  <<-EOSQL
     CREATE USER $REPLICATION_USER WITH REPLICATION PASSWORD '$REPLICATION_USER_PASSWORD' LOGIN CONNECTION LIMIT 15;
 
@@ -6,10 +9,11 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB"  <<-EOSQL
 
     GRANT CONNECT ON DATABASE $POSTGRES_DB TO $REPLICATION_USER;
 
-    CREATE EXTENSION postgres_fdw;
+    SELECT citus_set_coordinator_host('$PRIMARY_HOSTNAME', 5432);
+    SELECT citus_add_node('node1', 5432);
+    SELECT citus_add_node('node2', 5432);
 
-    CREATE SERVER $PRIMARY_HOSTNAME FOREIGN DATA WRAPPER postgres_fdw
-        OPTIONS (host '$PRIMARY_HOSTNAME', dbname '$POSTGRES_DB');
+    CREATE EXTENSION pg_stat_statements;
 
     CREATE TABLE customers (
         id serial, 
@@ -17,35 +21,11 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB"  <<-EOSQL
         lastname text,
         email text,
         country text,
-        age smallint,
-        date_created timestamp, 
-    ) PARTITION BY RANGE (age);
+        age smallint NOT NULL,
+        date_created timestamp
+    );
 
-    CREATE FOREIGN TABLE customer_child
-        PARTITION OF customers
-        FOR VALUES FROM (0) TO (12)
-        SERVER $PRIMARY_HOSTNAME;
-
-    CREATE FOREIGN TABLE customer_teenager
-        PARTITION OF customers
-        FOR VALUES FROM (12) TO (18)
-        SERVER $PRIMARY_HOSTNAME;
-
-    CREATE FOREIGN TABLE customer_youngadult
-        PARTITION OF customers
-        FOR VALUES FROM (18) TO (30)
-        SERVER $PRIMARY_HOSTNAME;
-
-    CREATE FOREIGN TABLE customer_adult
-        PARTITION OF customers
-        FOR VALUES FROM (30) TO (55)
-        SERVER $PRIMARY_HOSTNAME;
-
-    CREATE FOREIGN TABLE customer_elder
-        PARTITION OF customers
-        FOR VALUES FROM (55) TO (200)
-        SERVER $PRIMARY_HOSTNAME;
+    SELECT create_distributed_table('customers', 'age');
 
     INSERT INTO customers (firstname, date_created, age) VALUES ('Vuyisile', CURRENT_TIMESTAMP, 35);
-
 EOSQL
